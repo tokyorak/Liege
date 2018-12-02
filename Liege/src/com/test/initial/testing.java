cc54beaa58b28cba788a249a23053c677e491074
TokyMandimby <TokyMandimby@192.168.1.16>
2018-12-02 21:34:29 +0100

    First Commit

---
+++ Liege/src/com/test/initial/testing.java
@@ -0,0 +1,10 @@
+package com.test.initial;
+
+public class testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
