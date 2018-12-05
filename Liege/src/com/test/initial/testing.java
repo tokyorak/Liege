143b3641a9a84943907381b3ded7f889e654d168
TokyMandimby <TokyMandimby@192.168.1.16>
2018-12-05 15:38:27 +0100

    JFrame Hello world.

--- Liege/src/com/test/initial/testing.java
+++ Liege/src/com/test/initial/testing.java
@@ -1,10 +1,16 @@
 package com.test.initial;
 
+import javax.swing.JFrame;
+
 public class testing {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world");
+		//TR: JFrame test
+		JFrame frame = new JFrame("Hello World");
+		frame.setSize(500,500);
+		
+		frame.setVisible(true);
 	}
 
 }
